85e00b45edb9d07b3210a9fe2c6472fc6139ed27
zen-PC_FIXE <loris.pistilli.iut@gmail.com>
2019-04-05 00:28:30 +0200

    début création conflit 2.0
    je commence a comprendre la complexité de caser cette méthode dans une classe mais c'est un travail minutieux que je préfère décomposer en plusieurs petite étape c'est pour quoi je créé la méthode hit pour gérer les collisions (la méthode intersect de java pose problème car elle continue d’être appeler tant que n importe quelle partie d'un shape est en contact avec un adversaire) elle n'est pas fini mais déjà gère la collision entre un zombie vers une plant ou un projectile je dois faire l'autre sens, il y a aussi addInDP qui simplifie l'ajout dans les deadPool ces méthodes sont privée car elle son juste utile a conflit pour l'instant, je commence a voir trouble je crois que j'appercoie une autre dimension alors avant de mourir je vais me reposer

--- src/models/Entities.java
+++ src/models/Entities.java
@@ -1,7 +1,11 @@
 package models;
 
+import java.util.ArrayList;
+
+import plants.Bullet;
 import plants.Plant;
 import plants.Projectile;
+import views.BordView;
 import zombies.Zombie;
 
 public abstract class Entities {
@@ -55,10 +59,92 @@ public abstract class Entities {
 		this.life = life;
 	}
 
-	public void conflict(Entities e) {
-		life -= e.getDamage();
-		e.life -= damage;
+	/**
+	 * put an entitie in is respective deadPool
+	 * 
+	 * @param DPz deadPool for Zombies
+	 * @param DPp deadPool for Plant
+	 * @param DPb deadPool for Projectile
+	 */
+	private void addInDP(ArrayList<Zombie> DPz, ArrayList<Plant> DPp, ArrayList<Projectile> DPb) {
+		if (this instanceof Zombie) {
+			DPz.add((Zombie) this);
+		} else if (this instanceof Plant) {
+			DPp.add((Plant) this);
+		} else if (this instanceof Projectile) {
+			DPb.add((Projectile) this);
+		}
 	}
+	
+	/**
+	 * check if the object hit the e entitie
+	 * @param e is the entitie we want to compare the edge's positions
+	 * @return if this as touch e
+	 */
+	private boolean hit(Entities e,BordView view) {
+		
+		if (this instanceof Zombie) { // on gere une premiere possiblité --> une entité zombie touche une plante ou un projectile
+			float Zx1 =  this.getX(); //centre zombie
+			float Zx2 =  Zx1 - (Zombie.getSizeOfZombie()/3)-2; //bordure gauche zombie
+			if(e instanceof Projectile) {
+				float Bx1 =  e.getX(); //centre bullet
+				float Bx2 =  Bx1 + (Bullet.getSizeOfProjectile()/3)+2 ; //bordure droite bullet
+				if(view.lineFromY(this.getY()) == view.lineFromY(e.getY()) && (Bx1 < Zx2 && Zx2 <= Bx2) ) {
+					return true;
+				}
+			
+			}else if(e instanceof Plant) {
+				float Px1 =  e.getX(); //centre plante
+				float Px2 =  Px1 + (Plant.getSizeOfPlant()/3)+2 ; //bordure droite plante
+				if(view.lineFromY(this.getY()) == view.lineFromY(e.getY()) && (Px1 < Zx2 && Zx2 <= Px2)) {
+					return true;					
+				}
+			}
+		} else if ( this instanceof Plant || this instanceof Projectile  && e instanceof Zombie) { // on gere une seconde possiblité --> une entité plante ou projectile touche un zombie
+			return true;
+		}
+		return false;
+	}
+	/**
+	 * premier type conflit qui dois vite etre changer et améliorer
+	 * @param e entitées qui subira les dégats de l'entitée objet
+	 *                 utilisant la méthode et qui attaquera cette meme entitées
+	 *                 par la suite
+	 */
+	public void conflict(Entities e) { 
+		life -= e.getDamage(); 
+		e.life -= damage; 
+	} 
+	
+	/**
+	 * cette méthode a pour but de répartir les dégats aux différentes entitées du
+	 * jeu, une fois les dégat correctement attribuer et la vie des entitées mise a
+	 * jour elle aide par la suite a les redistribuer dans les différente deadpools
+	 * 
+	 * @param view vue sur la quelle ce joue le conflict (si on en met plusieur je saurai la faire marcher sur plusieur vues)
+	 * @param DPz deadPool for Zombies
+	 * @param DPp deadPool for Plant
+	 * @param DPb deadPool for Projectile
+	 * @param entities suite d'entitées qui subiront les dégats de l'entitée objet
+	 *                 utilisant la méthode et qui attaqueront cette meme entitées
+	 *                 tous ensemble
+	 */
+	public void conflict(BordView view,ArrayList<Zombie> DPz, ArrayList<Plant> DPp, ArrayList<Projectile> DPb, Entities... entities) {
+		for (Entities e : entities) {
+			if(this.hit(e, view)) {
 
+			life -= e.getDamage();
+			e.life -= damage;
+			if (e.getLife() <= 0) {
+				e.addInDP(DPz, DPp, DPb);
+			}else if(this.getLife() <= 0){ //si ils sont plusieur a le taper et que sa vie tombe a zero avant que les attaquant ne sois mort on empeche des echange de dégats(on en a besoin pour pas qu'une plante morte soit capable de tué après sa mort)
+				break;
+			}
+			}
+			
+		}
+		this.addInDP(DPz, DPp, DPb);
+
+	}
 
 }
